2142a3b434c00b7daea4b372bd11a5b3cf31720a
Luca Abatemarco <luca.abatemarco05@gmail.com>
2025-04-15 17:21:34 -0700

    Initial Commit

---
+++ LucaAbatemarcoResume/LucaAbatemarcoResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LucaAbatemarcoResumeApp.swift
+//  LucaAbatemarcoResume
+//
+//  Created by Luca Abatemarco on 15/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct LucaAbatemarcoResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LucaAbatemarcoResumeTests/LucaAbatemarcoResumeTests.swift
@@ -0,0 +1,17 @@
+//
+//  LucaAbatemarcoResumeTests.swift
+//  LucaAbatemarcoResumeTests
+//
+//  Created by Luca Abatemarco on 15/04/2025.
+//
+
+import Testing
+@testable import LucaAbatemarcoResume
+
+struct LucaAbatemarcoResumeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
